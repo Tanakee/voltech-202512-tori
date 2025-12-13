08c99fc38014f25fc1e5f7f49a255193ffad4a1e
Tanakee <tanaka.kate01@gmail.com>
2025-12-13 17:46:51 +0900

    フォントに関するCDNを読み込むように修正

---
+++ app/+html.tsx
@@ -0,0 +1,33 @@
+import { ScrollViewStyleReset } from 'expo-router/html';
+import { type PropsWithChildren } from 'react';
+
+/**
+ * This file is web-only and used to configure the root HTML for every
+ * web page during static rendering.
+ * The contents of this function only run in Node.js environments and
+ * do not have access to the DOM or browser APIs.
+ */
+export default function Root({ children }: PropsWithChildren) {
+  return (
+    <html lang="ja">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+        {/* 
+          This resets the default browser styles for ScrollView to match React Native's ScrollView.
+          It removes the scrollbars and sets the body to overflow: hidden.
+        */}
+        <ScrollViewStyleReset />
+
+        {/* Add Material Icons Font from Google Fonts CDN */}
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+
+        {/* Using raw CSS to hide the splash screen immediately on web to avoid flickering */}
+        <style dangerouslySetInnerHTML={{ __html: `body { background-color: #fff; }` }} />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
