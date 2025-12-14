40180507abc100d534a5ffbc077ec736d2329930
Tanakee <tanaka.kate01@gmail.com>
2025-12-14 12:46:56 +0900

    定義漏れ修正

--- config/firebase.ts
+++ config/firebase.ts
@@ -16,5 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+console.log("Firebase initialized:", app.name);
 export const db = getFirestore(app);
 // export const auth = getAuth(app);
